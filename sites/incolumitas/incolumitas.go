c22e6a312afa82ca9a06df354d41160f851fc7e8
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2022-12-05 11:13:54 -0500

    aoeu

---
+++ sites/incolumitas/incolumitas.go
@@ -0,0 +1,8 @@
+package incolumitas
+
+const Url = "https://bot.incolumitas.com"
+
+// CSSSelectors is a string mapping that contains the CSS selectors for the elements on bot.incolumitas.com.
+var CSSSelectors = map[string]string{
+	"userNameInput": `input[name="userName"]`,
+}
